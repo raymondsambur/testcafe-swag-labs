1bcc600853965edf2851a7ba6149274c889ae0ae
Raymond Sambur <raymondsambur@gmail.com>
2023-06-10 16:53:34 +0700

    Add new inventory data

---
+++ page_objects/components/inventory.js
@@ -0,0 +1,10 @@
+class InventoryData {
+    menu = {
+        allItems: "All Items",
+        about: "About",
+        logout: "Logout",
+        resetAppState: "Reset App State"
+    }
+}
+
+export default InventoryData
